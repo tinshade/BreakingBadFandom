436fbab345bb09de5be9ccd40150e5acc258416f
Abhishek Iyengar <tinshade98@gmail.com>
2020-07-07 12:16:07 +0530

    Add files via upload

---
+++ src/components/UI/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <nav className="bottom navbar-white bg-dark">
+            <div className="text-center pt-2" id="navbarCollapse">
+                <ul className="navbar-nav m-auto">
+                    <p style={{color: "white"}}>Made with <i style={{color: "red"}}>&hearts;</i> by <a href="https://github.com/tinshade">Abhishek Iyengar</a></p>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Footer
